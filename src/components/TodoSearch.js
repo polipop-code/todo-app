2ada14e39a49c6bd8a1e7221aecf6e8df9d17e3a
Jesus Politron <polipop.work@gmail.com>
2022-06-05 17:08:34 -0500

    added basic useState to the search component preparing for incoming changes

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -2,17 +2,29 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+	const [searchValue, setSearchValue] = React.useState("");
+
 	const onSearchValueChange = (event) => {
-		console.log(event.target.value);
+		// console.log(event.target.value);
+		setSearchValue(event.target.value);
 	};
 
-	return (
+	return [
 		<input
 			className="TodoSearch"
 			placeholder="Onion"
 			onChange={onSearchValueChange}
-		/>
-	);
+		/>,
+		<p>{searchValue}</p>,
+	];
 }
 
 export { TodoSearch };
+
+/*
+
+Creamos un estado con useState que por defecto va a ser un string vacío que estamos guardando en la variable searchValue, además tenemos una función para actualizar este valor que llamamos setSearchValue.
+
+Tambien tenemos una función onSearchValueChange que cada vez que hagamos algún cambio, que los usuarios escriban algo en el input de búsqueda, va a hacer además de un console.log va a llamar a esa función setSearchValue para actualizar nuestro searchValue (nuestro estado), esto lo estamos llamando en el método onChange de nuestro input y por que devemos de conectarlo por que asi nos lo pide React, estamos indicando que el valor de nuestro imput tiene que ser igual a nuestro estado (searchValue) que vamos ir actualizando con al función setSearchValue, por ultimo tambien queremos mostrar ese valor en un parrafo.
+
+*/
