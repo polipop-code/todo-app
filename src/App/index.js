98d5ec63d3a60ab55e5a3cd557bd2b3cc881a97c
Jesus Politron <polipop.work@gmail.com>
2022-06-08 18:34:38 -0500

    basic project organization

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,5 @@
 import React from "react";
-// import './App.css';
-
-import { TodoCounter } from "./components/TodoCounter";
-import { TodoSearch } from "./components/TodoSearch";
-import { TodoList } from "./components/TodoList";
-import { TodoItem } from "./components/TodoItem";
-import { CreateTodoButton } from "./components/CreateTodoButton";
+import { AppUI } from "./AppUI";
 
 const defaultTodos = [
 	{
@@ -71,28 +65,15 @@ function App() {
 	};
 
 	return (
-		<React.Fragment>
-			<TodoCounter
-				total={totalTodos}
-				completed={completedTodos}
-			/>
-			<TodoSearch
-				searchValue={searchValue}
-				setSearchValue={setSearchValue}
-			/>
-			<TodoList>
-				{searchedTodos.map((todo) => (
-					<TodoItem
-						key={todo.text}
-						text={todo.text}
-						completed={todo.completed}
-						onComplete={() => completeTodo(todo.text)}
-						onDelete={() => deleteTodo(todo.text)}
-					/>
-				))}
-			</TodoList>
-			<CreateTodoButton />
-		</React.Fragment>
+		<AppUI
+			totalTodos={totalTodos}
+			completedTodos={completedTodos}
+			searchValue={searchValue}
+			setSearchValue={setSearchValue}
+			searchedTodos={searchedTodos}
+			completeTodo={completeTodo}
+			deleteTodo={deleteTodo}
+		/>
 	);
 }
 
